27cbc4f01af571717054ce87ef800052a29ef341
jonathanle17 <112659204+jonathanle17@users.noreply.github.com>
2026-04-28 14:46:07 -0700

    Fix validation in sumValues and add length check in discountPrices

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add && num1.isNum() && num2.isNum()) {
+    if (add && Number.isFinite(num1) && Number.isFinite(num2)) {
         let result = 0;
 
         result = num1 + num2;
@@ -25,16 +25,15 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    if (discount >= 0 && discount <= 1) {
-        const discounted = []
-        const length = prices.length;
-        let discountedPrice = 0
+    const discounted = [];
+    const length = prices.length;
+    let discountedPrice = 0;
+    if (length > 0) {
         for(let i = 0; i < length; i++) {
             discountedPrice = prices[i] * (1 - discount);
             discounted.push(discountedPrice);
         }
     }
-
     return discounted;
 }
 
